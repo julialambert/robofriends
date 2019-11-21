ce950ad341afe7e8356730ec2ae25870c3a2c16a
Júlia Lambert <julialambert@MacBook-Pro-de-Julia.local>
2019-11-20 22:47:14 -0300

    Changing the title.

---
+++ build/precache-manifest.0cb9528384d5ca8409b5f60e3ddcc4fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb45a2bbb5c5574502ee9e37f73fc1bb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "65878f7f7f97efe97737",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "c584cc0b885051008fda",
+    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+  },
+  {
+    "revision": "65878f7f7f97efe97737",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "c584cc0b885051008fda",
+    "url": "/robofriends/static/js/main.6e624886.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
